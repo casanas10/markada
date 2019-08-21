8a3cfdde116354343836c4e3b737b03882c0de71
alex <casanas10@gmail.com>
2019-08-20 20:06:04 -0400

    updated env variable

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import datetime
 app = flask.Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-ENV = 'dev'
-# ENV = 'prod'
+# ENV = 'dev'
+ENV = 'prod'
 
 if ENV == 'dev':
     app.debug = True
