3f9e35ebc2106886699b8c864b6aa9d939ada89a
alex <casanas10@gmail.com>
2019-08-27 14:26:24 -0400

    uploaded model and setup form for article link input

--- api/views.py
+++ api/views.py
@@ -15,6 +15,7 @@ import string
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from tensorflow.python.keras.preprocessing.text import Tokenizer
 import pickle
+from sklearn.externals import joblib
 
 
 Bootstrap(app)
@@ -35,45 +36,20 @@ def clean_text(text):
 
 def sentiment_analysis(x):
 
-    #string to array
-    # x_array = [x]
-    # print(x_array)
+    # load the model from disk
+    # Load from file
+    with open("api/data/rf_model.pkl", 'rb') as file:
+        rf_model = pickle.load(file)
 
-    # #load the model
-    model = load_model('data/model.h5')
-
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
     x = [
         'March 15 (Reuters) - Mobile phone chip supplier Qualcomm Inc on Friday won a court victory against iPhone maker Apple Inc, with a jury in federal court in San Diego finding that Apple infringed on three of Qualcomm’s patents, a Qualcomm spokeswoman told Reuters. (Reporting by Stephen Nellis; Editing by Richard Chang)']
 
-    vectorizer = TfidfVectorizer(analyzer=clean_text)
-    vectorizer = vectorizer.fit(x)
+    tfidfVectorizer = joblib.load('api/data/tfidfVectorizer.pkl')
 
-    # load tokenizer
-    with open('data/tfidf_vect_fit.pickle', 'rb') as handle:
-        tfidf_vect_fit = pickle.load(handle)
-
-    tfidf_train = tfidf_vect_fit.transform(x)
+    tfidf_train = tfidfVectorizer.transform(x)
     x = pd.DataFrame(tfidf_train.toarray())
 
-    print(model.predict(x))
-
-    # #transform words into vector of features
-    # tfidf_vect = TfidfVectorizer(analyzer=clean_text)
-    # tfidf_vect_fit = tfidf_vect.fit(x_array)
-    #
-    # vect = tfidf_vect_fit.transform(x_array)
-    # y = pd.DataFrame(vect.toarray())
-
-    # vect = vectorizer.fit_transform(x_array).toarray()
-
-    # df = pd.DataFrame(X, columns=vectorizer.get_feature_names())
-
-    #predict sentiment
-    # print(model.predict(y))
-
+    print(rf_model.predict(x))
 
 
 @app.route("/", methods=["GET", "POST"])
