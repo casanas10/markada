bfa78077d169262ced9e874113d6a6bcb9603ad8
alex <casanas10@gmail.com>
2019-08-27 15:43:37 -0400

    update config

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,22 @@
 from flask import Flask
-from config import Config
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+# ENV = 'dev'
+ENV = 'prod'
+
+if ENV == 'dev':
+    from config import Config
+    app.config.from_object(Config)
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ["SQLALCHEMY_TRACK_MODIFICATIONS"]
+    app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+    app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
